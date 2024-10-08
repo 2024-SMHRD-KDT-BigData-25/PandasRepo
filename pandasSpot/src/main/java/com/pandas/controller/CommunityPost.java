bb4c4da1ad432a9d6f8449f9f22840fdb61edf5d
smhrd <smhrd@smart>
2024-10-08 11:24:04 +0900

    Merge branch 'master' of https://github.com/2024-SMHRD-KDT-BigData-25/PandasRepo

--- pandasSpot/src/main/java/com/pandas/controller/CommunityPost.java
+++ pandasSpot/src/main/java/com/pandas/controller/CommunityPost.java
@@ -7,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.pandas.model.Communities;
+import com.pandas.model.CommunityDAO;
 
 
 @WebServlet("/CommunityPost")
@@ -16,7 +20,31 @@ public class CommunityPost extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		request.setCharacterEncoding("UTF-8");
+		String comm_title = request.getParameter("comm_title");
+		String comm_content = request.getParameter("comm_content");
+		String comm_file = request.getParameter("comm_file");
+		String mem_id = request.getParameter("mem_id");
 		
+		Communities postComm = new Communities(comm_title, comm_content, comm_file, mem_id);
+		
+		CommunityDAO dao = new CommunityDAO();
+		int res = dao.CommunityPost(postComm);
+		
+		if (res != 1) {
+			System.out.println("자유게시판 게시물 등록!");
+			HttpSession session = request.getSession();
+			session.setAttribute("postComm", res);
+			response.sendRedirect("CommList.jsp");
+		}
+		else {
+			response.setContentType("text/html;charset=UTF-8");
+            response.getWriter().println("<html><head><title>게시물 등록 실패</title>");
+            response.getWriter().println("<script type='text/javascript'>");
+            response.getWriter().println("alert('입력하지 않은 사항이 있습니다!');"); // 팝업창
+            response.getWriter().println("window.history.back();"); // 이전 페이지로 돌아가기
+            response.getWriter().println("</script></head><body></body></html>");
+		}
 	}
 
 }
--- pandasSpot/src/main/java/com/pandas/model/CommunityDAO.java
+++ pandasSpot/src/main/java/com/pandas/model/CommunityDAO.java
@@ -25,4 +25,11 @@ public class CommunityDAO {
 			sqlSession.close();
 			return res;
 	}
+		
+		public int CommunityPost (Communities c) {
+			SqlSession sqlSession = sqlSessionFactory.openSession(true);
+			int res = sqlSession.selectOne("SpotMapper.CommunityPost",c);
+			sqlSession.close();
+			return res;
+		}
 }
